1a97d45c9fdd53f1383484ae0a5213f15ad2d0df
Alperen Avan <alperen_0150@hotmail.com>
2024-06-25 15:12:21 +0300

    DC Motor Control System

---
+++ Tinkercad Circuits Works/23-DC Motor Control System/53_dc_motor_control_system1.ino
@@ -0,0 +1,21 @@
+// C++ code
+//
+void setup()
+{
+  
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  
+}
+
+void loop()
+{
+  
+  digitalWrite(3,HIGH);
+  digitalWrite(2,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  
+}
